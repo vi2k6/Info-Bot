8f7d650b94245bba84e043767d365437a6b0ba0d
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-01 20:23:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 Bot = Client(
-        "Maintenance Bot",
+        "Info Bot",
         bot_token = os.environ["BOT_TOKEN"],
         api_id = int(os.environ["API_ID"]),
         api_hash = os.environ["API_HASH"]
