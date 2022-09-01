ed5aeba58a983c6c05694ea99bd2025319983f0e
Vɪᴠᴇᴋ <vivektp2006@gmail.com>
2022-09-01 19:25:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,6 @@
 import os
-from pyrogram import Client, filters
+from pyrogram import Client as Bot, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
-
-Bot = Client(
-    "Info Bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
-)
 
 START_TEXT = """<b>Hello {}
 
